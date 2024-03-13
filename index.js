b829dc18a33f787925b86ff3f7879a4f377e0ad7
Francisco Grimes da Silva <francisco.silva@digitro.corp>
2024-03-13 13:26:48 -0300

    Complemento

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function alteraCor() {
     const title = document.getElementById("title-ctn");
     title.style.backgroundColor = "red";
 }
+function voltaCor(){
+    const title = document.getElementById("title-ctn");
+    title.style.backgroundColor = "white";
+
+}
